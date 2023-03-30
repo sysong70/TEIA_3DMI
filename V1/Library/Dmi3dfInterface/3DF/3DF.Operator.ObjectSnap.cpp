c451afbd9ab18164f717179f89789dc3f8e235b3
Ken <iken@modview.com>
2023-03-30 20:14:55 +0900

    ObjectSnap

--- V1/Library/Dmi3dfInterface/3DF/3DF.Operator.ObjectSnap.cpp
+++ V1/Library/Dmi3dfInterface/3DF/3DF.Operator.ObjectSnap.cpp
@@ -110,7 +110,7 @@ namespace OSnap
 
 
 
-	const char* Format = "rgba, size=24 pixels";
+	const char* Format = "rgba, size=12 pixels";
 	int Width = 32;
 	int Height = 32;
 
@@ -246,7 +246,13 @@ void Operator::ObjectSnap::DrawObjectSnapPoint(_3DF::SelectionResults & cInItems
 //== Object Snap Point를 그리는 함수 ==================================================================
 void Operator::ObjectSnap::DrawCenterMark(const char * pchSegmentName, Point cPoint, COLORREF nColor, double dWeight)
 {
-#ifndef USE_IMAGE
+#ifdef USE_IMAGE
+	HC_Open_Segment(pchSegmentName);
+	{
+		HC_Insert_Image(cPoint.x, cPoint.y, cPoint.z, OSnap::Format, OSnap::Width, OSnap::Height, OSnap::Center.Buffer);
+	}
+	HC_Close_Segment();
+#else
 	HC_Open_Segment(pchSegmentName);
 	{
 /*
@@ -268,12 +274,6 @@ void Operator::ObjectSnap::DrawCenterMark(const char * pchSegmentName, Point cPo
 		HC_Insert_Marker(cPoint.x, cPoint.y, cPoint.z);
 	}
 	HC_Close_Segment();
-#else
-	HC_Open_Segment(pchSegmentName);
-	{
-		HC_Insert_Image(cPoint.x, cPoint.y, cPoint.z, OSnap::Format, OSnap::Width, OSnap::Height, OSnap::Center.Buffer);
-	}
-	HC_Close_Segment();
 #endif
 }
 
@@ -293,7 +293,13 @@ void Operator::ObjectSnap::DrawBox(const char * pchSegmentName, Point cPoint, CO
 
 void Operator::ObjectSnap::DrawEndPoint(const char * pchSegmentName, Point cPoint, COLORREF nColor, double dWeight)
 {
-#ifndef USE_IMAGE
+#ifdef USE_IMAGE
+	HC_Open_Segment(pchSegmentName);
+	{
+		HC_Insert_Image(cPoint.x, cPoint.y, cPoint.z, OSnap::Format, OSnap::Width, OSnap::Height, OSnap::Center.Buffer);
+	}
+	HC_Close_Segment();
+#else
 	HC_Open_Segment(pchSegmentName);
 	{
 		HC_Set_Visibility("marker = on");
@@ -304,18 +310,18 @@ void Operator::ObjectSnap::DrawEndPoint(const char * pchSegmentName, Point cPoin
 		HC_Insert_Marker(cPoint.x, cPoint.y, cPoint.z);
 	}
 	HC_Close_Segment();
-#else
-	HC_Open_Segment(pchSegmentName);
-	{
-		HC_Insert_Image(cPoint.x, cPoint.y, cPoint.z, OSnap::Format, OSnap::Width, OSnap::Height, OSnap::End.Buffer);
-	}
-	HC_Close_Segment();
 #endif
 }
 
 void Operator::ObjectSnap::DrawMidPoint(const char * pchSegmentName, Point cPoint, COLORREF nColor, double dWeight)
 {
-#ifndef USE_IMAGE
+#ifdef USE_IMAGE
+	HC_Open_Segment(pchSegmentName);
+	{
+		HC_Insert_Image(cPoint.x, cPoint.y, cPoint.z, OSnap::Format, OSnap::Width, OSnap::Height, OSnap::Center.Buffer);
+	}
+	HC_Close_Segment();
+#else
 	HC_Open_Segment(pchSegmentName);
 	{
 		HC_Set_Visibility("marker = on");
@@ -326,18 +332,18 @@ void Operator::ObjectSnap::DrawMidPoint(const char * pchSegmentName, Point cPoin
 		HC_Insert_Marker(cPoint.x, cPoint.y, cPoint.z);
 	}
 	HC_Close_Segment();
-#else
-	HC_Open_Segment(pchSegmentName);
-	{
-		HC_Insert_Image(cPoint.x, cPoint.y, cPoint.z, OSnap::Format, OSnap::Width, OSnap::Height, OSnap::Mid.Buffer);
-	}
-	HC_Close_Segment();
 #endif
 }
 
 void Operator::ObjectSnap::DrawNearPoint(const char * pchSegmentName, Point cPoint, COLORREF nColor, double dWeight)
 {
-#ifndef USE_IMAGE
+#ifdef USE_IMAGE
+	HC_Open_Segment(pchSegmentName);
+	{
+		HC_Insert_Image(cPoint.x, cPoint.y, cPoint.z, OSnap::Format, OSnap::Width, OSnap::Height, OSnap::Center.Buffer);
+	}
+	HC_Close_Segment();
+#else
 	HC_Open_Segment(pchSegmentName);
 	{
 		HC_Set_Variable_Marker_Size("3 oru");
@@ -348,12 +354,6 @@ void Operator::ObjectSnap::DrawNearPoint(const char * pchSegmentName, Point cPoi
 
 		HC_Set_Marker_Symbol("ObjectSnapNearPoint");
 		HC_Insert_Marker(cPoint.x, cPoint.y, cPoint.z);
-	}
-	HC_Close_Segment();
-#else
-	HC_Open_Segment(pchSegmentName);
-	{
-		HC_Insert_Image(cPoint.x, cPoint.y, cPoint.z, OSnap::Format, OSnap::Width, OSnap::Height, OSnap::Nearest.Buffer);
 	}
 	HC_Close_Segment();
 #endif
