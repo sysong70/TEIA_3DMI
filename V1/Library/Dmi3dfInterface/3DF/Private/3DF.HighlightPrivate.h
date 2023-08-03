25c81d3d25b74a88be3a336a33a6d07b2cbdfe5a
Ken <iken@modview.com>
2023-08-03 09:05:36 +0900

    Temp

---
+++ V1/Library/Dmi3dfInterface/3DF/Private/3DF.HighlightPrivate.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include <HBaseView.h>
+#include <HUtility.h>
+#include <HTools.h>
+#include <HSelectionSet.h>
+
+#include "../3DF.h"
+#include "../3DF.Kit.h"
+#include "../3DF.Control.h"
+#include "../3DF.Selection.h"
+
+#include "../3DF.BaseView.h"
+
+#include "../3DF.Math.h"
+
+#include <vector>
+
+OPEN_3DF_NAMESPACE
+
+class HighlightOptionsKitPrivate : public PrivateImpl
+{
+public:
+	void Copy(HighlightOptionsKitPrivate * pcInThat) {
+		strncpy(m_chInStyleName, pcInThat->m_chInStyleName, STYLE_BUFFER_SIZE);
+		strncpy(m_chInSecondaryStyleName, pcInThat->m_chInSecondaryStyleName, STYLE_BUFFER_SIZE);
+		m_nNotification = pcInThat->m_nNotification;
+	}
+
+	char m_chInStyleName[STYLE_BUFFER_SIZE];
+	char m_chInSecondaryStyleName[STYLE_BUFFER_SIZE];
+	int m_nNotification = -1;
+};
+
+class HighlightControlPrivate : public PrivateImpl
+{
+public:
+	HighlightControlPrivate() { m_eType = TDF::Type::HighlightControl; }
+
+	void Copy(HighlightControlPrivate * pcInThat) {
+		m_pcBaseView = pcInThat->m_pcBaseView;
+		m_pcWindow = pcInThat->m_pcWindow;
+		m_pcSelectionSet = pcInThat->m_pcSelectionSet;
+	}
+
+	// 	int SelectButtonDown(Point const & cInLocation, UINT const nFlags, SelectionResults & cOutResults);
+	// 	void HandleSelection(UINT const nFlags, SelectionResults & cOutResults);
+
+	WindowKey * GetWindow() { return (WindowKey *)m_pcWindow; }
+	const WindowKey * m_pcWindow = nullptr;
+
+	TDF::BaseView * GetBaseView() { return (TDF::BaseView *)m_pcBaseView; }
+	const TDF::BaseView * m_pcBaseView = nullptr;
+
+	HSelectionSet * m_pcSelectionSet = nullptr;
+};
+
+CLOSE_3DF_NAMESPACE
---
+++ V1/Library/DmiKernelInterface/DmiSignalAnalyzer.cpp
@@ -0,0 +1,60 @@
+﻿#include "stdafx.h"
+
+#include "DmiSignalAnalyzer.h"
+
+#include "../Signal/Signal.h"
+#include "../Common/Common_Define.h"
+
+using namespace Json;
+using namespace std::chrono;
+
+#define MSG_FILE_IMPORT_INFORMATION				(WM_USER + 1)
+
+DmiSignalAnalyzer theCmdAnalyzer;
+
+DmiSignalAnalyzer::DmiSignalAnalyzer()
+{
+}
+
+//== 명령어 처리 부분 =================================================================================
+
+// 1. 최초 명령어 처리 분기
+void DmiSignalAnalyzer::ExecuteSignal(const wchar_t * pchBuffer)
+{
+	Json::Object cObject;
+	Reader::ReadObject((wchar_t *&) pchBuffer, cObject);
+
+	// CString strText = cObject.ToString() + L"\r\n";
+	// OutputDebugString(strText);
+
+	int nTarget = cObject.GetInteger(SKW_TARGET);
+
+	switch((Signal::Target)nTarget)
+	{
+		case Signal::Target::Application: // CApp
+		case Signal::Target::View:
+			m_3DF_Interface.ExecuteSignal(cObject);
+			break;
+
+		default:
+			assert(false);
+			break;
+	}
+}
+
+void DmiSignalAnalyzer::SetSignalCallback(DmiSignalCallback lpfnSignalCallback) {
+	m_lpfnSignalCallback = lpfnSignalCallback;
+
+	//m_cHpsInterface.SetSignalCallback(lpfnSignalCallback);
+	m_3DF_Interface.SetSignalCallback(lpfnSignalCallback);
+}
+/*
+
+void DmiSignalAnalyzer::SendCommandToMainFrame(UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	if(nullptr == m_lpfnMainFrameSendMessageCallback) {
+		return;
+	}
+
+	m_lpfnMainFrameSendMessageCallback(nMsg, wParam, lParam);
+}	*/
